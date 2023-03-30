df90f445a3e92bb032999a90b030f1195b2bd72a
Olia Brizgina <olvitbriz@gmail.com>
2023-03-30 20:37:47 +0400

    initial

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remain() {
+    }
+}
