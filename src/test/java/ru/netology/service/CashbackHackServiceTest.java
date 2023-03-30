b75cf0263aba351cdab830261cf40508f6844611
Olia Brizgina <olvitbriz@gmail.com>
2023-03-30 21:06:40 +0400

    add test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    void remainIfAmountThousand() {
+        CashbackHackService cashbackHackService= new CashbackHackService();
+        int amount=1000;
+        int actual= cashbackHackService.remain(amount);
+        int expected=0;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void remainIfAmountNineHundred() {
+        CashbackHackService cashbackHackService= new CashbackHackService();
+        int amount=900;
+        int actual= cashbackHackService.remain(amount);
+        int expected=100;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void remainIfAmountThousandAndOneHundred() {
+        CashbackHackService cashbackHackService= new CashbackHackService();
+        int amount=1100;
+        int actual= cashbackHackService.remain(amount);
+        int expected=900;
+        assertEquals(expected, actual);
     }
 }
